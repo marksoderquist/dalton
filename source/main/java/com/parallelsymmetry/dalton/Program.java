34c6c308a957f85cb2d18b27c09943ce574e74ee
msoderquist <msoderquist@mastercontrol.com>
2025-05-28 10:54:00 -0600

    Update version number to 1.5u03 and refactor Program class to improve readability and structure

--- source/main/java/com/parallelsymmetry/dalton/Program.java
+++ source/main/java/com/parallelsymmetry/dalton/Program.java
@@ -3,39 +3,40 @@ package com.parallelsymmetry.dalton;
 import com.parallelsymmetry.service.Service;
 import com.parallelsymmetry.utility.Parameters;
 import com.parallelsymmetry.utility.setting.Settings;
+import lombok.Getter;
+
+import java.io.Reader;
 
 public class Program extends Service {
 
-	private final WeatherStation station;
-
+	@Getter
 	private final DavisReader reader;
+
+	@Getter
+	private final WeatherStation station;
 
 	public static void main( String[] commands ) {
 		new Program().process( commands );
 	}
 
 	public Program() {
-		Settings products = getSettings().getNode( "products" );
-		Settings markSoderquistNetPublisherSettings = products.getNode( "mark.soderquist.net.weather" );
+		reader = new DavisReader();
+		station = new BluewingWeatherStation();
+	}
+
+	@Override
+	protected void startService( Parameters parameters ) throws Exception {
+		Settings markSoderquistNetPublisherSettings = getSettings().getNode( "products/mark.soderquist.net.weather" );
 		markSoderquistNetPublisherSettings.put( "tryhere", "with this value" );
 		markSoderquistNetPublisherSettings.flush();
 
-		station = new BluewingWeatherStation();
 		station.addPublisher( new MarkSoderquistNetPublisher(markSoderquistNetPublisherSettings.get( "authorization", null )) );
 		// This is being sent to Perform from the mark.soderquist.net weather server
 		//station.addPublisher( new PerformWeatherPublisher( this ) );
 		station.addPublisher( new WeatherUndergroundPublisher( this ) );
 
-		reader = new DavisReader();
 		reader.addWeatherStation( station );
-	}
 
-	public WeatherStation getWeatherStation() {
-		return station;
-	}
-
-	@Override
-	protected void startService( Parameters parameters ) throws Exception {
 		reader.start();
 	}
 
